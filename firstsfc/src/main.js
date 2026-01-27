9183ce5ff039ff9f89dd35f7164222b6a173a211
Jan Samuel G. Sultan <jgsultan@student.apc.edu.ph>
2026-01-27 17:12:50 +0800

    Test

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
 import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
