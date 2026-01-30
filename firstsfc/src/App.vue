4984221236619aa74efe3581af8dfc9cb834bee3
Jan Samuel G. Sultan <jgsultan@student.apc.edu.ph>
2026-01-30 13:06:17 +0800

    Task 03 Add view and insert comment/visitor feedback functionality to your website

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,22 +1,11 @@
-<script setup>
-import { ref, onMounted } from 'vue'
-import { supabase } from './lib/supabaseClient'
-
-const instruments = ref([])
-
-async function getInstruments() {
-  const { data } = await supabase.from('instruments').select()
-  instruments.value = data
-}
-
-onMounted(() => {
-   console.log("App mounted! Fetching data...") // If you don't see this in F12, the app crashed before this line.
-   getInstruments()
-})
-</script>
-
 <template>
-  <ul>
-    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
-  </ul>
+  <h1>Food</h1>
+  <personal-profile/>
+  <food-item/>
+  <comment-form/>
+  <comment/>
+   
+
 </template>
+
+<script></script>
--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -1,12 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import FoodItem from './components/FoodItem.vue'
+import FoodItem2 from './components/FoodItem2.vue'
+import CommentForm from './components/CommentForm.vue'
+import Comment from './components/Comment.vue'
 
-// Vite will now find these in your firstsfc/node_modules
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 import PersonalProfile from './components/PersonalProfile.vue'
 
 const app = createApp(App)
+app.component1('food-item', FoodItem)
+app.component1('food-item2', FoodItem2)
 app.component('personal-profile', PersonalProfile)
+app.component('comment-form', CommentForm)
+app.component('comment', Comment)
 app.mount('#app')
